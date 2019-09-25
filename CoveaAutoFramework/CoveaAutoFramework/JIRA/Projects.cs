8030e65b3308d816af20f42912712c8c45c315e5
Gerald Anene <anenegerald@gmail.com>
2019-09-25 05:39:41 +0100

    First commit

---
+++ CoveaAutoFramework/CoveaAutoFramework/JIRA/Projects.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoveaAutoFramework.JIRA
+{
+    public class Projects
+    {
+        public List<Project> Options { get; set; }
+    }
+}
